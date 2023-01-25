aa0260d58d7b932501f01fc2103d1ed50c53c859
DgPrometeo <danielgarciasan93@gmail.com>
2023-01-25 22:56:06 +0100

    Javascript initial

---
+++ script.js
@@ -0,0 +1,15 @@
+var proyectos = document.getElementById("proyectos");
+var habilidades = document.getElementById("habilidades");
+var contacto = document.getElementById("contacto");
+
+proyectos.addEventListener("click", function() {
+    alert("Mostrando proyectos");
+});
+
+habilidades.addEventListener("click", function() {
+    alert("Mostrando habilidades");
+});
+
+contacto.addEventListener("click", function() {
+    alert("Mostrando contacto");
+});
